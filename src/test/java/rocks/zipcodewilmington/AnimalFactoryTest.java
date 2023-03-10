2dbeb892f1f1233527b369ce65513e858b172ce9
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-10 17:36:49 -0500

    CatHouseTest finished

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,49 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+
+    public void createDogTest(){
+        //Given
+        String name = "Clifford";
+        Date birthDate = new Date(2009 - 1900, 3, 20);
+
+        //When
+
+        DogHouse.add(AnimalFactory.createDog(name, birthDate));
+        int amountDogs = DogHouse.getNumberOfDogs();
+
+        //Then
+        Assert.assertEquals(1, amountDogs);
+    }
+
+    @Test
+
+    public void createCatTest(){
+        //Given
+        String name = "Puss";
+        Date birthDate = new Date(2019 - 1900, 2, 2);
+
+        //When
+
+        CatHouse.add(AnimalFactory.createCat(name, birthDate));
+        int amountCats = CatHouse.getNumberOfCats();
+
+        //Then
+        Assert.assertEquals(1, (int) CatHouse.getNumberOfCats());
+    }
 }
--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,15 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.net.Authenticator;
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +19,82 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    @Test
+
+    public void addCatTest(){
+        //Given
+        CatHouse.clear();
+        Date birthDate = new Date(2006 - 1900, 5, 3);
+        Cat cat = AnimalFactory.createCat("Damian", birthDate);
+
+        //When
+        CatHouse.add(cat);
+
+        //Then
+        Assert.assertEquals(1, (int)CatHouse.getNumberOfCats());
+    }
+
+    @Test
+    public void removeIdTest(){
+        //Given
+        CatHouse.clear();
+        Date birthDate = new Date(2006 - 1900, 5, 3);
+        Cat cat = AnimalFactory.createCat("Damian", birthDate);
+
+        //When
+        CatHouse.remove(cat);
+
+        //Then
+        Assert.assertEquals(0, (int)cat.getId());
+
+    }
+
+    @Test
+    public void removeCatTest(){
+        //Given
+        CatHouse.clear();
+        Date birthDate = new Date(2006 - 1900, 5, 3);
+        Cat cat = AnimalFactory.createCat("Damian", birthDate);
+        CatHouse.add(cat);
+
+        //When
+        CatHouse.remove(cat);
+
+        //Then
+        Assert.assertEquals(0, (int)CatHouse.getNumberOfCats());
+
+
+
+    }
+
+    @Test
+    public void getCatByIdTest(){
+        //Given
+        CatHouse.clear();
+        Date birthDate = new Date(2006 - 1900, 5, 3);
+        Cat cat = AnimalFactory.createCat("Damian", birthDate);
+        CatHouse.add(cat);
+
+        //When
+        int thisId = cat.getId();
+
+        //Then
+        Assert.assertEquals(cat, CatHouse.getCatById(thisId));
+
+    }
+
+    @Test
+    public void getNumCatsTest(){
+        //Given
+        CatHouse.clear();
+        Date birthDate = new Date(2006 - 1900, 5, 3);
+        Cat cat = AnimalFactory.createCat("Damian", birthDate);
+
+        //When
+        CatHouse.add(cat);
+
+        //Then
+        Assert.assertEquals(1, (int)CatHouse.getNumberOfCats());
+    }
 }
